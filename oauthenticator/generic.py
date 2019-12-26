644826dc9af677790d5d46b6864dfd100cdf34ff
Shane Starcher <shanestarcher@gmail.com>
2019-12-26 12:39:31 -0800

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! skeleton of sphinx docs (#316)

--- oauthenticator/generic.py
+++ oauthenticator/generic.py
@@ -97,8 +97,7 @@ class GenericOAuthenticator(OAuthenticator):
 
     async def authenticate(self, handler, data=None):
         code = handler.get_argument("code")
-        # TODO: Configure the curl_httpclient for tornado
-        http_client = AsyncHTTPClient(force_instance=True, defaults=dict(validate_cert=False))
+        http_client = AsyncHTTPClient(force_instance=True, defaults=dict(validate_cert=self.tls_verify))
 
         params = dict(
             redirect_uri=self.get_callback_url(handler),
